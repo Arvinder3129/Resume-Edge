8acaecac247e5697fe260de849941dd53340fdc2
Arvinder3129 <157027953+Arvinder3129@users.noreply.github.com>
2026-09-02 11:33:35 +0530

    Update app.run configuration for production
    Change app.run to use environment variable for port and disable debug mode.

--- main.py
+++ main.py
@@ -147,6 +147,9 @@ def request_entity_too_large(_error):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
 
 
